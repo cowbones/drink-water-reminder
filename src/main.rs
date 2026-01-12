a2ff84f8852c978a780db8db9b888ecd287b1be1
cowbones <me@devoncasey.com>
2026-01-12 17:36:37 -0500

    fix: service now loops indefinitely instead of relying on restart of the service

--- src/main.rs
+++ src/main.rs
@@ -3,25 +3,28 @@ use notify_rust::Notification;
 use std::thread;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let now = Local::now();
-    let period = Duration::minutes(15); // arbitrary time between sips
+    let period = Duration::minutes(15);
     let sleep_duration = period.to_std()?;
 
+    println!("Hydration reminder started");
     println!("Reminder period set to every {:?}", sleep_duration);
 
-    println!("Waiting...");
-    thread::sleep(sleep_duration);
+    loop {
+        thread::sleep(sleep_duration);
 
-    if let Err(e) = Notification::new()
-        .summary("Drink water!")
-        .body("Dumbass.")
-        .icon("water-notify")
-        .show()
-    {
-        println!("Push notification at {} failed! Error: {}", now.format("%I:%M %p"), e);
-    } else {
-        println!("Notification sent at {}", now.format("%I:%M %p"));
+        let now = Local::now();
+
+        if let Err(e) = Notification::new()
+            .summary("Drink water!")
+            .body("Dumbass.")
+            .icon("drink-water-reminder")
+            .show()
+        {
+            eprintln!(
+                "Push notification at {} failed! Error: {}", now.format("%I:%M %p"), e
+            );
+        } else {
+            println!("Notification sent at {}", now.format("%I:%M %p"));
+        }
     }
-
-    Ok(())
 }
